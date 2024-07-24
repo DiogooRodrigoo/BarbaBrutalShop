23f6c65beb6f3a7c9c2d93ea0ac9a307a3b1a01c
Diogo Rodrigo Pedreira Galvão <diogo.rgalvao@outlook.com>
2024-07-24 17:19:46 -0300

    Desenvolvimento dos componentes de serviços

--- apps/front-end/src/app/(paginas)/(landing)/page.tsx
+++ apps/front-end/src/app/(paginas)/(landing)/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import TituloSlogan from "@/components/landing/TituloSlogan";
+import NossosServicos from "@/components/servico/NossosServicos";
+import ContainerComBacground from "@/components/shared/ContainerComBackground";
 
 export default function Landing() {
   return (
     <div>
-      Landing
       <TituloSlogan />
+
+      <ContainerComBacground imagem="/banners/servicos.webp">
+        <NossosServicos />
+      </ContainerComBacground>
     </div>
   );
 }
---
+++ apps/front-end/src/components/servico/NossosServicos.tsx
@@ -0,0 +1,33 @@
+import useServicos from "@/data/hooks/useServicos";
+import { useRouter } from "next/navigation";
+import Titulo from "../shared/Titulo";
+import { Servico } from "@barba/core";
+import ServicoItem from "./ServicoItem";
+
+export default function NossosServicos() {
+  const router = useRouter();
+  const { servicos } = useServicos();
+
+  function iniciarAgendamento() {
+    router.push("/agendamento");
+  }
+
+  return (
+    <div className="flex flex-col gap-16">
+      <Titulo
+        tag="Serviços"
+        principal="Do Classico do Rock"
+        secundario="Cabelo afiado, barba de lenhador e mãos de motoqueiro, tudo ao som de rock pesado !"
+      />
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
+        {servicos.map((servico: Servico) => (
+          <ServicoItem
+            key={servico.id}
+            servico={servico}
+            onClick={iniciarAgendamento}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
